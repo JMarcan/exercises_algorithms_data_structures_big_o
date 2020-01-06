fbc5b2b9376e0647eba8f84e7c360fe7da51ea1b
JMarcan <jan.marcan@protonmail.com>
2020-01-06 09:24:31 +0100

    feat: implement Task 0

--- Task0.py
+++ Task0.py
@@ -1,15 +1,23 @@
 """
 Read file into texts and calls.
-It's ok if you don't understand how to read files.
 """
+
 import csv
+
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
+    
+    # store the first text line, so it can be later printed for Task 0
+    first_text = texts[0]
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+    
+    # store the first calls line, so it can be later printed for Task 0
+    first_call = calls[0]
 
 
 """
@@ -19,4 +27,7 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
+
+print("First record of texts, <{0}> texts <{1}> at time <{2}>".format(first_text[0], first_text[1], first_text[2]))
+print("First record of calls, <{0}> calls <{1}> at time <{2}>".format(first_call[0], first_call[1], first_call[2]))
 
