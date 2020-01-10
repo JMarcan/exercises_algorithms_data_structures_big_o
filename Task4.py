84ee54b5a2d9c7e7290d5cdff52ddd72cb4648d9
JMarcan <jan.marcan@protonmail.com>
2020-01-10 06:48:23 +0100

    feat: implement Task 4

--- Task4.py
+++ Task4.py
@@ -1,6 +1,5 @@
 """
 Read file into texts and calls.
-It's ok if you don't understand how to read files.
 """
 import csv
 
@@ -24,4 +23,41 @@ Print a message:
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
+
+possible_telemarketers = []
+
+# gather list of all unique numbers who called to anybody
+for record in calls:
+    calling_number = record[0]
+    if calling_number not in possible_telemarketers:
+        possible_telemarketers.append(calling_number)
+
+# to identify possible telemarketers
+# eliminate from the list numbers that send texts, receive texts or receive incoming calls
+
+# eliminate the ones that receive calls
+for record in calls:
+    receiving_number = record[1]
+    
+    if receiving_number in possible_telemarketers:
+        possible_telemarketers.remove(receiving_number)
+
+# eliminate the ones that send or receive texts
+for record in texts:
+    sending_number = record[0]
+    receiving_number = record[1]
+    
+    if sending_number in possible_telemarketers:
+        possible_telemarketers.remove(sending_number)
+        
+    if receiving_number in possible_telemarketers:
+        possible_telemarketers.remove(receiving_number)
+
+possible_telemarketers.sort()
+
+# Answer to the Task 4
+print("Those numbers could be a telemarketers:")
+for number in possible_telemarketers:
+    print(number)
+    
 
